d5ad5aab6386984631044cba68562dd5f2bf826a
Oleg B <this.is.my.address.for.mail@ya.ru>
2021-01-02 03:57:49 +0300

    Added: jQuery, smooth scroll to sections on main page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,12 @@
 // дождаться полной загрузки страницы
 $(function() {
-
     let intro = $("#intro");
     let header = $("#header");
     let introH;
     let headerH;
 
 
-
-    /* Header color */
+    // Header color
     function headerScroll() {
         introH = intro.innerHeight();
         headerH = header.innerHeight();
@@ -20,15 +18,19 @@ $(function() {
             header.removeClass("header--dark");
         }
     }
-
-    // сразу при загрузке страницы вызвать один раз
     headerScroll();
-    // отслеживать скролл и поворот
     $(window).on("scroll resize", () => headerScroll());
 
 
 
-    /* Smooth scroll to sections */
+    // Smooth scroll to sections
+    $("[data-scroll]").on("click", function(event) { 
+        event.preventDefault();
+        
+        let scrollEl = $(this).data("scroll");      // вернёт готовый #id
+        let scrollElPos = $(scrollEl).offset().top; // позиция элемента от верха страницы
 
+        $("html, body").animate({ scrollTop: scrollElPos - headerH }, 500);
+    });
 
 });
