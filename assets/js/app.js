68be6687724686808734f7363caf787319fe9b1d
Oleg B <this.is.my.address.for.mail@ya.ru>
2021-01-04 00:05:50 +0300

    Added: jQuery, scroll spy

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,4 +33,27 @@ $(function() {
         $("html, body").animate({ scrollTop: scrollElPos - headerH }, 500);
     });
 
+
+
+    // ScrollSpy
+    function scrollSpy() {
+        let scrollTop = $(this).scrollTop();
+        $("[data-scrollspy]").each(function() {
+            let $this = $(this);
+            let sectionId = $this.data('scrollspy');
+            let sectionOffset = $this.offset().top - ($(window).height() * 0.3);
+
+            if (scrollTop >= sectionOffset) {
+                $(`#nav :not([data-scroll='${sectionId}'])`).removeClass('active');
+                $(`#nav [data-scroll='${sectionId}']`).addClass('active');
+            }
+
+            if (scrollTop < $(window).height() * 0.3) {
+                $('#nav [data-scroll]').removeClass('active');
+            }
+        })
+    }
+    scrollSpy();
+    $(window).on("scroll", () => scrollSpy());
+
 });
